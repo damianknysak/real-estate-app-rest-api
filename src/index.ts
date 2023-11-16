767f716697cbb6d080b3ae9988dee9fc07579e8e
damianknysak <damian11rs@onet.pl>
2023-11-16 19:36:49 +0100

    basic-structure

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 dotenv.config();
 /**
  * App Variables
@@ -25,10 +27,14 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
+
 /**
  * Server Activation
  */
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-  console.log(`First tests ${PORT}`);
 });
